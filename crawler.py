19266a3d65ded1569dadcb96cd5a617c35ca4506
nick <iamamiracle55@gmail.com>
2015-01-18 00:16:25 +0800

    import feedparser

--- crawler.py
+++ crawler.py
@@ -28,7 +28,32 @@ def httpCrawler(url):
 	results={}
 	results={'title':len(title),'t2':t2,'link':len(link),'pubdate':len(pubDate),'descr':len(descr),'content':len(item)}
 	return results
+
+def httpDetail(url):
+	'''
+	title link description content
+	'''
+	content=httpRequest(url)#发送请求
+	parser=etree.XMLParser(strip_cdata=False)
+
+	root = etree.XML(content,parser)
+	descr=root.xpath(u"//description")
+	soup=BeautifulSoup(content)
+	item=soup.findAll('content:encoded')
+	title=root.xpath(u"//title") 
+	link=root.xpath(u"//link")
+	pubDate=root.xpath(u"//pubDate")
+
+	article=title[0].text
 	
+	content=[]
+	for t in range(len(title)):
+		newtime=datetime.datetime.strptime(pubDate[t].text[:25],"%a, %d %b %Y %H:%M:%S")
+		newtime=newtime.strftime('%Y年%m月%d日 %H:%M:%S')
+		content.append({'title':title[t].text,'link':link[t].text,'pubdate':newtime,'descr':descr[t].text})
+
+	temp={'article':article,'content':content}
+	return temp
 
 def httpRequest(url):
 	'''
--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,16 @@
 # encoding: utf-8
 from flask import Flask,request,render_template
-import crawler
+import crawler,feedparse
 app=Flask(__name__)
 @app.route('/')
 def index():
 	#处理提交的url并测试
 	url=request.args.get('url')
 	if url:
-		results=crawler.httpCrawler(url)
-		# results=""
-		return render_template('index.html',results=results,url=url)
+		temp=feedparse.api(url)
+		return render_template('detail.html',article=temp['article'],content=temp['content'])
 	else:
 		return render_template('index.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
